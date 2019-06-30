230e0d78b06cca23cb72a4216f8d4ae438d228a0
jfqd <jfqd@blun.org>
2019-06-30 18:35:05 +0200

    fix nagios dependencies

--- copy/var/zoneinit/includes/46-apache.sh
+++ copy/var/zoneinit/includes/46-apache.sh
@@ -2,3 +2,5 @@
 
 # Setup hostname
 sed -i "s:HOSTNAME:$(hostname):g" /opt/local/etc/httpd/vhosts/00-default.conf
+svcadm enable -r svc:/pkgsrc/apache:default
+svcadm enable -r svc:/pkgsrc/nagios:default
