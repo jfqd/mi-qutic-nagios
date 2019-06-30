6f262455c2ae61e46699aa9f7381d6f440813e3d
jfqd <jfqd@blun.org>
2019-06-30 19:31:16 +0200

    remove cronjob for now

--- copy/var/zoneinit/includes/19-cronjobs-www.sh
+++ copy/var/zoneinit/includes/19-cronjobs-www.sh
@@ -1,3 +1,3 @@
 # create cronjob for nexcloud script
-echo "0,15,30,45 * * * * /usr/bin/find /var/tmp/ -name sess_* -cmin +120 -exec rm '{}' +" >> /var/spool/cron/crontabs/www
-echo "# End" >> /var/spool/cron/crontabs/www
+# echo "0,15,30,45 * * * * /usr/bin/find /var/tmp/ -name sess_* -cmin +120 -exec rm '{}' +" >> /var/spool/cron/crontabs/www
+# echo "# End" >> /var/spool/cron/crontabs/www
