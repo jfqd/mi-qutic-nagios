420ac0683bd8f1e566a46554b96782e1c78a58dc
jfqd <jfqd@blun.org>
2019-06-30 19:13:13 +0200

    add basic auth for apache

--- copy/var/zoneinit/includes/46-apache.sh
+++ copy/var/zoneinit/includes/46-apache.sh
@@ -2,5 +2,16 @@
 
 # Setup hostname
 sed -i "s:HOSTNAME:$(hostname):g" /opt/local/etc/httpd/vhosts/00-default.conf
+
+# create htpasswd file
+if mdata-get apache_htpasswd 1>/dev/null 2>&1; then
+  user=$(mdata-get apache_htpasswd | tr ":"  "\n" | sed -n 1p)
+  pass=$(mdata-get apache_htpasswd | tr ":"  "\n" | sed -n 2p)
+  crypt=$(openssl passwd -apr1 $pass)
+  echo "$user:$crypt" > /opt/local/etc/nagios/htpasswd.users
+  chown www:root /opt/local/etc/nagios/htpasswd.users
+  chmod 0640 /opt/local/etc/nagios/htpasswd.users
+fi
+
 svcadm enable -r svc:/pkgsrc/apache:default
 svcadm enable -r svc:/pkgsrc/nagios:default
